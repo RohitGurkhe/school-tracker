07535a67fbeec39d72b2972d828dbcd4818cbaf0
BDB IT - Rohit Gurkhe <rohitg@BDBIndia.local>
2026-04-03 16:00:07 +0530

    added default route

--- server.js
+++ server.js
@@ -151,3 +151,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log("🚀 Server running on port " + PORT);
 });
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/dashboard.html');
+});
